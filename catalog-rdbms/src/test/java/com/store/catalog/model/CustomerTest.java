679ae869f02838690d70ad835d975ca81119e5f2
zouheircadi <zouheir.cadi@gmail.com>
2017-11-19 18:38:02 +0100

    add comments for students

--- catalog-rdbms/src/test/java/com/store/catalog/model/CustomerTest.java
+++ catalog-rdbms/src/test/java/com/store/catalog/model/CustomerTest.java
@@ -1,7 +1,13 @@
 package com.store.catalog.model;
 
+import com.store.catalog.common.exception.CheckException;
+import org.hibernate.cfg.NotYetImplementedException;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.*;
 
 /**
@@ -9,8 +15,52 @@ import static org.junit.Assert.*;
  */
 public class CustomerTest {
 
-    @Test
-    public void testMatchPassword() throws Exception {
-        throw new RuntimeException("not yet implemented");
+
+    //fixtures ...
+    //declare a Customer class
+
+
+    //fixtures ...
+    //Create a fixture that will be called before the execution of each test method
+    //This fixture is responsible of the customer Class declared above
+
+
+    /**
+     * Test 1
+     */
+    public void verifyWhenPasswordNullReturnException() throws Exception {
+        throw new NotYetImplementedException();
     }
+
+    /**
+     * Test 2
+     */
+    public void verifyWhenPasswordEmptyReturnException() throws Exception {
+        throw new NotYetImplementedException();
+    }
+
+
+    /**
+     * Test 3
+     */
+    public void verifyWhenPasswordDontMatchReturnException() throws Exception {
+        throw new NotYetImplementedException();
+    }
+
+
+    /**
+     * Test 4
+     */
+    public void verifyWhenPasswordMatchReturnTrue() throws Exception {
+        throw new NotYetImplementedException();
+    }
+
+
+    /**
+     * Test 5
+     */
+    public void verifyWhenPasswordNullReturnMsg() throws Exception {
+        throw new NotYetImplementedException();
+    }
+
 }
